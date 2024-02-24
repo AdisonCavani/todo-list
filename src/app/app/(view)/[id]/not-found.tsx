ba956bbe783c1b229867987aa329e7747a27cb23
Adison Cavani <adriansrodon@tuta.io>
2024-02-24 19:52:40 +0100

    feat: add 404 infographics

--- src/app/app/(view)/[id]/not-found.tsx
+++ src/app/app/(view)/[id]/not-found.tsx
@@ -1,5 +1,19 @@
+import Graphics from "@images/not_found.svg";
+import Image from "next/image";
+
 function NotFound() {
-  return <p>List not found</p>;
+  return (
+    <section className="hidden w-full grow flex-col items-center justify-center gap-y-10 px-3 pb-24 pt-8 sm:px-6 sm:pb-8 lg:flex">
+      <Image src={Graphics} alt="Infographics" width={300} />
+
+      <div className="max-w-64 space-y-2 text-center text-sm">
+        <p className="font-semibold">Hmmm... Page not found!</p>
+        <p className="text-muted-foreground">
+          Sorry, we couldn&apos;t find the page you&apos;re looking for.
+        </p>
+      </div>
+    </section>
+  );
 }
 
 export default NotFound;
--- src/app/app/(view)/page.tsx
+++ src/app/app/(view)/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 function Page() {
   return (
     <section className="hidden w-full grow flex-col items-center justify-center gap-y-10 px-3 pb-24 pt-8 sm:px-6 sm:pb-8 lg:flex">
-      <Image src={Graphics} alt="Infographics" width={300} className="" />
+      <Image src={Graphics} alt="Infographics" width={300} />
 
       <div className="max-w-64 space-y-2 text-center text-sm">
         <p className="font-semibold">Hmmm... Where are my tasks?</p>
