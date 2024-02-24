dcd1035bfbe51307ab253f5b67545e8637ab148e
Adison Cavani <adriansrodon@tuta.io>
2024-02-24 20:00:44 +0100

    refactor: error page props type

--- src/app/app/(view)/[id]/error.tsx
+++ src/app/app/(view)/[id]/error.tsx
@@ -4,11 +4,10 @@ import { Button } from "@ui/button";
 import type Error from "next/error";
 
 type Props = {
-  error: Error;
+  error: Error & { digest?: string };
   reset: () => void;
 };
 
-// TODO: replace this with proper Next.js type
 function ErrorComponent({ reset }: Props) {
   return (
     <div className="-mx-3 -mb-24 -mt-8 flex h-full grow flex-col items-center justify-center bg-destructive p-4 text-destructive-foreground sm:-mx-6 sm:-mb-8">
