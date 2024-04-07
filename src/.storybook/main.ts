aa6d1b4c41dde8c054fe891f6c4018b70736b9cb
Adison Cavani <adriansrodon@tuta.io>
2024-04-07 16:00:19 +0200

    chore: move stories folder

--- src/.storybook/main.ts
+++ src/.storybook/main.ts
@@ -1,7 +1,7 @@
 import type { StorybookConfig } from "@storybook/nextjs";
 
 const config: StorybookConfig = {
-  stories: ["../stories/**/*.stories.@(ts|tsx)", "../app/**/*.stories.tsx"],
+  stories: ["../app/**/*.stories.tsx"],
   addons: [
     "@storybook/addon-a11y",
     "@storybook/addon-links",
