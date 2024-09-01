87ffdb50a684863103cdab749cc60e0b920c56d1
Adrian Środoń <srodonadrian@proton.me>
2024-09-01 16:41:13 +0200

    fix: storybook build error

--- src/.storybook/main.ts
+++ src/.storybook/main.ts
@@ -1,7 +1,7 @@
 import type { StorybookConfig } from "@storybook/nextjs";
 
 const config: StorybookConfig = {
-  stories: ["../dash/**/*.stories.tsx"],
+  stories: ["../app/**/*.stories.tsx"],
   addons: [
     "@storybook/addon-a11y",
     "@storybook/addon-links",
